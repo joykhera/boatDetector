7248d50ee8e9ad51e045f5b6fc5b1f66d7100747
joykhera <joykhera@gmail.com>
2023-10-23 14:10:30 -0400

    added boat class

---
+++ boat.py
@@ -0,0 +1,48 @@
+import cv2
+
+class Boat:
+    def __init__(self, box, id=None):
+        """
+        Initialize a Boat object.
+
+        :param box: tuple of bounding box coordinates (x1, y1, x2, y2) for the boat.
+        :param id: optional unique identifier for the boat.
+        """
+        self.id = id
+        self.prev_coords = None
+        self.update_coords(box)
+        self.speed = 0.0
+
+    def update_coords(self, box):
+        """
+        Update the boat's coordinates and compute its speed based on the change in position.
+
+        :param new_box: tuple of bounding box coordinates (x1, y1, x2, y2) for the boat.
+        """
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        self.coords = {
+            'x1': x1,
+            'y1': y1,
+            'x2': x2,
+            'y2': y2,
+            'xmid': abs(x2 - x1) / 2 + min(x2, x1),
+            'ymid': abs(y2 - y1) / 2 + min(y2, y1)
+        }
+
+        # Update speed based on distance traveled and a time unit (assuming 1 for simplicity)
+        if self.prev_coords:
+            distance = ((self.coords['xmid'] - self.prev_coords['xmid']) ** 2 + (self.coords['ymid'] - self.prev_coords['ymid']) ** 2) ** 0.5
+            self.speed = distance  # Here, speed = distance as time is assumed to be 1 unit. You can adjust as needed.
+
+        # Update the previous coordinates to the current ones before updating current to new
+        self.prev_coords = self.coords
+
+    def __str__(self):
+        return f"Boat ID: {self.id}, Current Coords: {self.coords}, Previous Coords: {self.prev_coords}, Speed: {self.speed}"
+    
+    def draw(self, frame, confidence):
+        cv2.rectangle(frame, (self.coords['x1'], self.coords['y1']), (self.coords['x2'], self.coords['y2']), (0, 255, 0), 2)  # Green color for the box
+        label = f"{self.id}: {confidence:.2f}"
+        cv2.putText(frame, label, (self.coords['x1'], self.coords['y1'] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        # cv2.putText(frame, confidence, (self.coords['x1'], self.coords['y1'] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 from ultralytics import YOLO
 import cv2
-import pyautogui
-from PIL import ImageGrab
+from boat import Boat
 
 # load yolov8 model
 model = YOLO('yolov8x.pt')
-
 cap = cv2.VideoCapture(0)
+objectToDetect = 'boat'
+boats = {}
 
 ret = True
 # read frames
 while ret:
     ret, frame = cap.read()
+    frame_ = frame
 
     if ret:
-
-        # detect objects
-        # track objects
         results = model.track(frame, persist=True)
-        # print(results[0]._keys)
-        # labels = results[0].boxes.labels if results.boxes is not None else []
-        # print(labels)
-        # print(type(results[0]))
+
         for box in results[0].boxes:
             detectedObject = results[0].names[box.cls[0].item()]
-            print(detectedObject)
-
-            if detectedObject == 'keyboard':
-                print('keyboard detected')
+            confidence = box.conf.item()
+            print(detectedObject, confidence)
+            
+            if detectedObject == objectToDetect and confidence > 0.3:
+                print(box)
+                print(f'{objectToDetect} detected')
+                if box.id:
+                    id = box.id.item()
+                    
+                    if id not in boats:
+                        boat = Boat(box, id)
+                        boats[id] = boat
+                    else:
+                        boat = boats[id]
+                
+                    boat.update_coords(box)
+                    boat.draw(frame, confidence)
+                
+                
                 cv2.imwrite('./screenshot.png', frame)
-                # myScreenshot = pyautogui.screenshot()
-                # myScreenshot.save(r'./screenshot.png')
 
-        # plot results
-        # cv2.rectangle
-        # cv2.putText
-        frame_ = results[0].plot()
 
         # visualize
         cv2.imshow('frame', frame_)
