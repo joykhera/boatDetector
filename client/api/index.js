dac867eaef95463853b375f436177f5e449120e9
joykhera <joykhera@gmail.com>
2023-10-30 14:04:52 -0400

    try remove publid in requests

--- client/api/index.js
+++ client/api/index.js
@@ -4,6 +4,9 @@ import fs from 'fs/promises'
 import expressGraphQl from 'express-graphql'
 import schema from '../schema/index.js'
 
+
+dotenv.config({ path: '../.env' });
+
 const app = express()
 const port = process.env.EXPRESS_PORT;
 
@@ -22,11 +25,11 @@ app.get('/', async (req, res, next) => {
 })
 
 app.get('/about', async (req, res, next) => {
-    res.send(await fs.readFile('public/about.html', 'utf8'))
+    res.send(await fs.readFile('about.html', 'utf8'))
 })
 
 app.get('/docs', async (req, res, next) => {
-    res.send(await fs.readFile('public/docs.html', 'utf8'))
+    res.send(await fs.readFile('docs.html', 'utf8'))
 })
 
 app.listen(port, () => {
