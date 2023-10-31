057b04f940f867a52af76458f0a5c18d24995a18
joykhera <joykhera@gmail.com>
2023-10-31 16:02:35 -0400

    change server to app

--- client/app.js
+++ client/app.js
@@ -2,8 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import fs from 'fs/promises'
 import expressGraphQl from 'express-graphql'
-import schema from '../schema/index.js'
-
+import schema from './schema/index.js'
 
 dotenv.config({ path: '../.env' });
 
@@ -25,11 +24,11 @@ app.get('/', async (req, res, next) => {
 })
 
 app.get('/about', async (req, res, next) => {
-    res.send(await fs.readFile('about.html', 'utf8'))
+    res.send(await fs.readFile('public/about.html', 'utf8'))
 })
 
 app.get('/docs', async (req, res, next) => {
-    res.send(await fs.readFile('docs.html', 'utf8'))
+    res.send(await fs.readFile('public/docs.html', 'utf8'))
 })
 
 app.listen(port, () => {
