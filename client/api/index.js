9e97bf05620dac4d765e93b6c2a6ecde3fb7eec7
joykhera <joykhera@gmail.com>
2023-10-31 16:35:21 -0400

    try index again

--- client/api/index.js
+++ client/api/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import fs from 'fs/promises'
 import expressGraphQl from 'express-graphql'
-import schema from './schema/index.js'
+import schema from '../schema/index.js'
 
 dotenv.config({ path: '../.env' });
 
@@ -12,24 +12,12 @@ const port = process.env.EXPRESS_PORT;
 app.use(express.static("public"));
 
 app.use(
-    '/graphql',
+    '/api/graphql',
     expressGraphQl.graphqlHTTP({
         schema: schema,
         graphiql: true
     })
 );
-
-// app.get('/', async (req, res, next) => {
-//     res.send(await fs.readFile('index.html', 'utf8'))
-// })
-
-// app.get('/about', async (req, res, next) => {
-//     res.send(await fs.readFile('public/about.html', 'utf8'))
-// })
-
-// app.get('/docs', async (req, res, next) => {
-//     res.send(await fs.readFile('public/docs.html', 'utf8'))
-// })
 
 app.listen(port, () => {
     console.log(`Server running at: http://localhost:${port}/`)
